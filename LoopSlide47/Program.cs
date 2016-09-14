f98502fb6a8c884276b6e1cbe4ae34e4049c48ca
KurtWeber <weberkrt@msn.com>
2016-09-13 20:58:34 -0400

    Slide 47 complete

---
+++ LoopSlide47/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopSlide47
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Initialize variables
+            int number;
+
+            // Loop to test user input
+            do
+            {
+                Console.Write("Please enter a integer to see if you've won: ");
+                number = int.Parse(Console.ReadLine());
+
+                // To print you lost only if number isn't divisible by 3
+                if (number % 3 != 0)
+                {
+                    Console.WriteLine("\nYou Lost");
+                    Console.WriteLine("\nPlease try again\n");
+                }
+            } while (number % 3 != 0);
+            Console.WriteLine("\nYou Won!");
+        }
+    }
+}
